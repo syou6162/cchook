18dd190f42c79200ba14e82bf531baa8491b87f9
Yasuhisa Yoshida <syou6162@gmail.com>
2025-07-26 03:13:41 +0900

    feat: ExitStatusアクション機能を追加
    - 全アクション構造体にExitStatusフィールドを追加
    - outputアクションのデフォルトExitStatusを2に設定
    - ExitStatus 2の場合はstderrに出力してos.Exit(2)で終了
    - getExitStatus関数でデフォルト値制御を実装
    
    PreToolUseでoutputアクションを使用した場合、ツール実行を
    ブロックし、警告メッセージがClaude Codeに確実に伝わるように改善

--- actions.go
+++ actions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func executeNotificationAction(action NotificationAction, input *NotificationInput, rawJSON interface{}) error {
 	switch action.Type {
@@ -11,7 +14,13 @@ func executeNotificationAction(action NotificationAction, input *NotificationInp
 		}
 	case "output":
 		message := unifiedTemplateReplace(action.Message, rawJSON)
-		fmt.Println(message)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
 	}
 	return nil
 }
@@ -25,7 +34,13 @@ func executeStopAction(action StopAction, input *StopInput, rawJSON interface{})
 		}
 	case "output":
 		message := unifiedTemplateReplace(action.Message, rawJSON)
-		fmt.Println(message)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
 	}
 	return nil
 }
@@ -39,7 +54,13 @@ func executeSubagentStopAction(action SubagentStopAction, input *SubagentStopInp
 		}
 	case "output":
 		message := unifiedTemplateReplace(action.Message, rawJSON)
-		fmt.Println(message)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
 	}
 	return nil
 }
@@ -53,7 +74,65 @@ func executePreCompactAction(action PreCompactAction, input *PreCompactInput, ra
 		}
 	case "output":
 		message := unifiedTemplateReplace(action.Message, rawJSON)
-		fmt.Println(message)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
 	}
 	return nil
+}
+
+func executePreToolUseAction(action PreToolUseAction, input *PreToolUseInput, rawJSON interface{}) error {
+	switch action.Type {
+	case "command":
+		cmd := unifiedTemplateReplace(action.Command, rawJSON)
+		if err := runCommand(cmd); err != nil {
+			return err
+		}
+	case "output":
+		message := unifiedTemplateReplace(action.Message, rawJSON)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
+	}
+	return nil
+}
+
+func executePostToolUseAction(action PostToolUseAction, input *PostToolUseInput, rawJSON interface{}) error {
+	switch action.Type {
+	case "command":
+		cmd := unifiedTemplateReplace(action.Command, rawJSON)
+		if err := runCommand(cmd); err != nil {
+			return err
+		}
+	case "output":
+		message := unifiedTemplateReplace(action.Message, rawJSON)
+		exitStatus := getExitStatus(action.ExitStatus, "output")
+		if exitStatus == 2 {
+			fmt.Fprintf(os.Stderr, "%s\n", message)
+			os.Exit(2)
+		} else {
+			fmt.Println(message)
+		}
+	}
+	return nil
+}
+
+// getExitStatus returns the exit status for the given action type
+// Default for "output" actions is 2, others default to 0
+func getExitStatus(exitStatus *int, actionType string) int {
+	if exitStatus != nil {
+		return *exitStatus
+	}
+	if actionType == "output" {
+		return 2
+	}
+	return 0
 }
