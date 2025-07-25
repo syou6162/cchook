7557f47908e0df2d15ab6a5fa452ea12513a2c95
Yasuhisa Yoshida <syou6162@gmail.com>
2025-07-25 20:34:46 +0900

    refactor: 未使用コードを削除してコードベースを整理
    - 未使用関数を削除: getFieldValue, valueToString
    - 未使用インポートを削除: reflect パッケージ
    
    これによりgolangci-lintのunusedエラー2個を解決し、
    リフレクション依存を除去してコードを簡素化

--- utils.go
+++ utils.go
@@ -4,38 +4,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"reflect"
 	"strings"
 )
 
 // parseInput関数は parser.go に移動
-
-// リフレクションを使って構造体のフィールド値を取得するヘルパー関数
-func getFieldValue(data interface{}, fieldName string) string {
-	if data == nil {
-		return ""
-	}
-
-	value := reflect.ValueOf(data)
-	// ポインタの場合は実体を取得
-	for value.Kind() == reflect.Ptr || value.Kind() == reflect.Interface {
-		if value.IsNil() {
-			return ""
-		}
-		value = value.Elem()
-	}
-
-	if value.Kind() != reflect.Struct {
-		return ""
-	}
-
-	field := value.FieldByName(fieldName)
-	if !field.IsValid() {
-		return ""
-	}
-
-	return valueToString(field)
-}
 
 // 共通マッチャーチェック関数
 func checkMatcher(matcher string, toolName string) bool {
@@ -49,36 +21,6 @@ func checkMatcher(matcher string, toolName string) bool {
 		}
 	}
 	return false
-}
-
-// 値を文字列に変換する関数
-func valueToString(value reflect.Value) string {
-	// ポインタの場合は実体を取得
-	for value.Kind() == reflect.Ptr || value.Kind() == reflect.Interface {
-		if value.IsNil() {
-			return ""
-		}
-		value = value.Elem()
-	}
-
-	if !value.IsValid() {
-		return ""
-	}
-
-	switch value.Kind() {
-	case reflect.String:
-		return value.String()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return fmt.Sprintf("%d", value.Int())
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return fmt.Sprintf("%d", value.Uint())
-	case reflect.Float32, reflect.Float64:
-		return fmt.Sprintf("%g", value.Float())
-	case reflect.Bool:
-		return fmt.Sprintf("%t", value.Bool())
-	default:
-		return fmt.Sprintf("%v", value.Interface())
-	}
 }
 
 func replacePreToolUseVariables(command string, input *PreToolUseInput, rawJSON interface{}) string {
